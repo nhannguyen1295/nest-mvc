b46271be67c27294c4b9bb5685890e73d12ab3ea
Nguyen Linh Thanh Nhan <nguyenlinhthanhnhan@outlook.com>
2022-08-29 17:12:36 +0700

    Add database configure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import {ProductsController} from "./product.controller";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ConfigModule} from "@nestjs/config";
+import {MysqlDbConfigureService} from "./config/mysql-db-configure.service";
+import {AppService} from "./app.service";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: MysqlDbConfigureService,
+    })],
   controllers: [AppController, ProductsController],
-  providers: [AppService],
+  providers: [AppService, MysqlDbConfigureService],
 })
 export class AppModule {}
---
+++ src/config/mysql-db-configure.service.ts
@@ -0,0 +1,26 @@
+﻿import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
+
+@Injectable()
+export class MysqlDbConfigureService implements TypeOrmOptionsFactory {
+
+    constructor(private configService: ConfigService) {}
+
+    createTypeOrmOptions(): TypeOrmModuleOptions {
+        console.log('DATABASE_HOST', this.configService.get<string>('DATABASE_HOST'))
+        console.log('DATABASE_UID', this.configService.get<string>('DATABASE_UID'))
+        console.log('DATABASE_PWD', this.configService.get<string>('DATABASE_PWD'))
+        console.log('DATABASE_NAME', this.configService.get<string>('DATABASE_NAME'))
+        console.log('DATABASE_SYNC', this.configService.get<string>('DATABASE_SYNC') === 'true')
+        return {
+            type: 'mysql',
+            host: this.configService.get<string>('DATABASE_HOST'),
+            username: this.configService.get<string>('DATABASE_UID'),
+            password: this.configService.get<string>('DATABASE_PWD'),
+            database: this.configService.get<string>('DATABASE_NAME'),
+            entities: ["dist/**/*.entity{.ts,.js}"],
+            synchronize:this.configService.get<string>('DATABASE_SYNC') === 'true'
+        };
+    }
+}
