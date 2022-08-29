e4f4ceb1bc9e5766feab2916b64c4a05057edcfc
Nguyen Linh Thanh Nhan <nguyenlinhthanhnhan@outlook.com>
2022-08-29 17:22:33 +0700

    Remove console.log

--- src/config/mysql-db-configure.service.ts
+++ src/config/mysql-db-configure.service.ts
@@ -8,11 +8,6 @@ export class MysqlDbConfigureService implements TypeOrmOptionsFactory {
     constructor(private configService: ConfigService) {}
 
     createTypeOrmOptions(): TypeOrmModuleOptions {
-        console.log('DATABASE_HOST', this.configService.get<string>('DATABASE_HOST'))
-        console.log('DATABASE_UID', this.configService.get<string>('DATABASE_UID'))
-        console.log('DATABASE_PWD', this.configService.get<string>('DATABASE_PWD'))
-        console.log('DATABASE_NAME', this.configService.get<string>('DATABASE_NAME'))
-        console.log('DATABASE_SYNC', this.configService.get<string>('DATABASE_SYNC') === 'true')
         return {
             type: 'mysql',
             host: this.configService.get<string>('DATABASE_HOST'),
